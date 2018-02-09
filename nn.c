a29ac25c471da0cd3d8ff854da92dd58b04c31ab
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-02-09 16:07:22 +0530

    Create nn.c

---
+++ nn.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    
+int n,i;
+printf("ENter the n value");
+ scanf("%d",&n);
+ for(i=0;i<=5;i++)
+ {
+     printf("\n %d",n*i);
+ }
+getch();
+}
